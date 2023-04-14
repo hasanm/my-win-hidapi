da34cb0b85e505be8229ca7a12cdc2fd7484bf74
Mahmudul Hasan <cs.mahmud@gmail.com>
2023-04-13 22:39:07 -0600

    Using hid_write

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -28,8 +28,8 @@ MainWindow::MainWindow(QWidget *parent)
 
 
   pushButton = new QPushButton(QString("Push"));
-  connect(pushButton, &QPushButton::clicked, this, &MainWindow::onPush); 
-  
+  connect(pushButton, &QPushButton::clicked, this, &MainWindow::onPush);
+
   topLayout->addWidget(quitButton);
   topLayout->addWidget(pushButton);
 
@@ -54,24 +54,56 @@ MainWindow::MainWindow(QWidget *parent)
   setCentralWidget(root);
 }
 
+// Vendor_ID Product_ID Usage USAGE_PAGE
+
+// KBD75v2
+// 0x4B42 0x6061
+// 19266 24673 6 1
 
 void MainWindow::onPush()
 {
-  QString output{"Enumeration Begins\n"};
-  textArea->setText(output);
   hid_device *handle;
   struct hid_device_info *devs;
+  unsigned char buf[256];
+  int res;
+
+
+
+
+  QString output{"Enumeration Begins\n"};
+  textArea->setText(output);
 
   if (hid_init()) {
     return;
   }
   devs = hid_enumerate(0x0, 0x0);
-  int i = 0; 
+  int i = 0;
   while(devs){
     i++;
-    output.append(QString("%1 %2 %3 %4 %5\n").arg(i).arg(devs->vendor_id).arg(devs->product_id).arg(devs->path).arg(devs->serial_number));
-    devs = devs->next; 
+    output.append(QString("%1 %2 %3 %4 %5 %6\n").arg(i).arg(devs->vendor_id).arg(devs->product_id).arg(devs->usage).arg(devs->usage_page).arg(devs->serial_number));
+    devs = devs->next;
   }
   hid_free_enumeration(devs);
+  handle = hid_open(0x4B42, 0x6061, NULL);
   textArea->setText(output);
+
+  if(!handle) {
+    hid_exit();
+  } else {
+    memset(buf, 0x00, sizeof(buf));
+    buf[0] = 0x01;
+    buf[1] = 0x80;
+
+    int res = hid_write(handle, buf, 17);
+
+    if (res < 0) {
+      output.append(QString("%1\n").arg(hid_error(handle)));
+    }
+
+    hid_close(handle);
+    hid_exit();
+  }
+
+  textArea->setText(output);
+
 }
